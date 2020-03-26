46195d0161eeffb9cbb70c7215121a8f3ea989b2
zhouhaobit <329998641@qq.com>
2020-03-26 16:43:38 +0800

    项目上传

---
+++ src/wxPayClass.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of wxPayClass
+ *
+ * @author Administrator
+ */
+namespace zhouhaobit\selfwxpayclass;
+
+class wxPayClass {
+    public function test(){
+        echo "test composer";
+    }
+}
